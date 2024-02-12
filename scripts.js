fe4ef43eccd3a2b08d34c6a8e57271142b8a93d1
Nick Flores <joseflores2@csumb.edu>
2024-02-12 09:58:30 -0800

    First commit

---
+++ scripts.js
@@ -0,0 +1,63 @@
+let firstTime = true;
+//display cake after button click
+document.getElementById('bottom-center-button').addEventListener('click', () =>{
+     document.getElementById('container').style.display = 'block';
+});
+
+//display conf
+document.getElementById("bottom-center-button").addEventListener("click", () =>{
+     //alert("buttonclicked")
+    //TODO: Confetti after cake animation finish
+    //confetti();
+
+     // let canvas = document.createElement("canvas");
+     // canvas.width = 600;
+     // canvas.height = 600;
+     // let container = document.getElementsByClassName("button-wrapper")[0];
+     // container.appendChild(canvas);
+ 
+    //let confetti_button = confetti.create();
+     
+     //confetti_button({
+     // do this for 30 seconds
+
+
+  // launch a few confetti from the left edge
+ // for(let i=0; i< 5; i++ ){
+     if(firstTime){
+          setTimeout(function (){
+               confetti({
+                    particleCount: 200,
+                    angle: 60,
+                    spread: 55,
+                    origin: { x: 0 }
+                  });
+                  // and launch a few from the right edge
+                  confetti({
+                    particleCount: 200,
+                    angle: 120,
+                    spread: 55,
+                    origin: { x: 1 }
+                  });
+           }, 6000)
+           firstTime = false;
+     }else{
+          confetti({
+               particleCount: 100,
+               angle: 60,
+               spread: 55,
+               origin: { x: 0 }
+             });
+             // and launch a few from the right edge
+             confetti({
+               particleCount: 100,
+               angle: 120,
+               spread: 55,
+               origin: { x: 1 }
+             });
+     }
+     
+  //}
+  // keep going until we are out of time
+    
+});
