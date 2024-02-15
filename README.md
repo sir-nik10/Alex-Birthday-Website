be2ddf58fe07db273d936b676b754c9bd9a81700
Nick Flores <joseflores2@csumb.edu>
2024-02-15 13:14:30 -0800

    cleaned up code

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ localhost:8000
 ```
 
 ![BdayWebsite](https://github.com/sir-nik10/Alex-Birthday-Website/assets/53841083/3e0d0bbd-fccf-49a7-a5ce-0567e6283313)
+
--- scripts.js
+++ scripts.js
@@ -1,77 +1,69 @@
-
-
 let firstTime = true;
-//display cake after button click
-document.getElementById('bottom-center-button').addEventListener('click', () =>{
-     document.getElementById('container').style.display = 'block';
-    //console.log("anything");
+//create new audio context (For Chrome)
+let audioCtx = new AudioContext();
+let button = document.getElementById('bottom-center-button');
+//on click 
+button.addEventListener('click', () =>{
+    //display cake animation
+    document.getElementById('container').style.display = 'block';
+    //play music on click first time 
+    //play confetti after cake animation
     if(firstTime){
-     let audioCtx = new AudioContext();
-     let audio = new Audio("music/Bongo Cat - Birthday Song (TRAP REMIX).mp3");
-     console.log(audioCtx);
-     console.log(audioCtx.state);
-     console.log(audio)
-     audio.volume =.25;
-     audio.play()
+      playAudio();
+      confettiAnimation(firstTime);
+      firstTime=false;
+    }else{
+      confettiAnimation(firstTime);
+    }
+     
+});
+
+function playAudio(){
+  let audio = new Audio("music/Bongo Cat - Birthday Song (TRAP REMIX).mp3");
+  audio.volume =.15;
+  //wait for cake
+  setTimeout(function(){
+    audio.play()
+  },4000);
+  setTimeout(function(){
+    audio.pause();
+  },24000);
+}
+
+function confettiAnimation(firstTime){
+   //launch confetti at end of cake animation
+   if(firstTime){
+    setTimeout(function (){
+        //launch from left edge
+         confetti({
+              particleCount: 200,
+              angle: 60,
+              spread: 55,
+              origin: { x: 0 }
+            });
+            // and launch a few from the right edge
+            confetti({
+              particleCount: 200,
+              angle: 120,
+              spread: 55,
+              origin: { x: 1 }
+            });
+     }, 6000)
      firstTime = false;
-    } 
-    
-});
-
-//display conf
-document.getElementById("bottom-center-button").addEventListener("click", () =>{
-     //alert("buttonclicked")
-    //TODO: Confetti after cake animation finish
-    //confetti();
-
-     // let canvas = document.createElement("canvas");
-     // canvas.width = 600;
-     // canvas.height = 600;
-     // let container = document.getElementsByClassName("button-wrapper")[0];
-     // container.appendChild(canvas);
- 
-    //let confetti_button = confetti.create();
-     
-     //confetti_button({
-     // do this for 30 seconds
-
-
-  // launch a few confetti from the left edge
- // for(let i=0; i< 5; i++ ){
-     if(firstTime){
-          setTimeout(function (){
-               confetti({
-                    particleCount: 200,
-                    angle: 60,
-                    spread: 55,
-                    origin: { x: 0 }
-                  });
-                  // and launch a few from the right edge
-                  confetti({
-                    particleCount: 200,
-                    angle: 120,
-                    spread: 55,
-                    origin: { x: 1 }
-                  });
-           }, 6000)
-           firstTime = false;
-     }else{
-          confetti({
-               particleCount: 100,
-               angle: 60,
-               spread: 55,
-               origin: { x: 0 }
-             });
-             // and launch a few from the right edge
-             confetti({
-               particleCount: 100,
-               angle: 120,
-               spread: 55,
-               origin: { x: 1 }
-             });
-     }
-     
-  //}
-  // keep going until we are out of time
-    
-});
+    }else{
+      //launch from left edge
+      confetti({
+        particleCount: 100,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 }
+       });
+       // and launch a few from the right edge
+       confetti({
+          particleCount: 100,
+          angle: 120,
+          spread: 55,
+          origin: { x: 1 }
+        });
+  }
+}
